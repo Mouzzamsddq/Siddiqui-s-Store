ba16232575eece173dafc61fcf6460d6ae8dc6b0
mohazzamsddq07333@gmail.com <mohazzamsddq07333@gmail.com>
2022-06-12 12:34:05 +0530

    enabled view binding

--- app/src/main/java/com/example/siddiquisstore/MainActivity.kt
+++ app/src/main/java/com/example/siddiquisstore/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.siddiquisstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.siddiquisstore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
